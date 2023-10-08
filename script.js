89510071e4da97fd0752274eef6f39f7b95e915e
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-10-09 03:30:37 +0530

    Add basic math operations

---
+++ script.js
@@ -0,0 +1,23 @@
+let operandLeft = 0;
+let operandRight = 0;
+let operation = '';
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2
+}
+
+function operate(opr, operand1, operand2){
+
+}
