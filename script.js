ea035364a206a5b70d21808dddc188e3f201382b
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-10-11 05:32:49 +0530

    Make the expression on clicking operators

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let operandLeft = 0;
 let operandRight = 0;
 let operation = '';
+let newOperand = false;
+let expression = [];
 
 function add(num1, num2){
     return num1+num2;
@@ -33,14 +35,38 @@ function operate(opr, operand1, operand2){
     return result;
 }
 
+function makeExpression(e){
+    console.log(e)
+    let currentOperand = Number.parseInt(display.innerText);
+    let currentOperator = '';
+    switch(e.srcElement.dataset.operator){
+        case '+': currentOperator = '+'; break;
+        case '-': currentOperator = '-'; break;
+        case '*': currentOperator = '*'; break;
+        case '/': currentOperator = '/'; break;
+    }
+    expression.push(currentOperand);
+    expression.push(currentOperator);
+    newOperand = true;
+    display.innerText = '';
+}
+
 let numBtns = document.querySelectorAll('.num-btn');
 const display = document.querySelector('#display');
 
 numBtns.forEach(numBtn=>{
     numBtn.addEventListener('click',()=>{
-        if(display.innerText=='000'){
+        if(display.innerText=='000' || newOperand){
             display.innerText = '';    
         }
         display.innerText = display.innerText + numBtn.dataset.num;
     });
+})
+
+let oprBtns = document.querySelectorAll('.opr-btn');
+
+oprBtns.forEach(opr=>{
+    if(opr.dataset.operator != 'IGNR'){
+        opr.addEventListener('click',makeExpression)   
+    }
 })
