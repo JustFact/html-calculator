a7962caa24e813980622ffab96a80ba5f8a57b42
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-10-28 05:12:57 +0530

    Remove main branch evaluation logic (BODMAS)

--- script.js
+++ script.js
@@ -17,84 +17,84 @@ function divide(num1, num2){
     return num1/num2
 }
 
-function operate(opr, operand1, operand2){
-    let result = 0;
-    switch(opr){
-        case '+': result = add(operand1,operand2);
-        break;
-        case '-': result = subtract(operand1,operand2);
-        break;
-        case '*': result = multiply(operand1,operand2);
-        break;
-        case '/': result = divide(operand1,operand2);
-        break;
-    }
-    return result;
-}
+// function operate(opr, operand1, operand2){
+//     let result = 0;
+//     switch(opr){
+//         case '+': result = add(operand1,operand2);
+//         break;
+//         case '-': result = subtract(operand1,operand2);
+//         break;
+//         case '*': result = multiply(operand1,operand2);
+//         break;
+//         case '/': result = divide(operand1,operand2);
+//         break;
+//     }
+//     return result;
+// }
 
-function makeExpression(e){
-    let currentOperand = Number.parseInt(display.innerText);
-    let currentOperator = '';
-    switch(e.srcElement.dataset.operator){
-        case '+': currentOperator = '+'; break;
-        case '-': currentOperator = '-'; break;
-        case '*': currentOperator = '*'; break;
-        case '/': currentOperator = '/'; break;
-    }
-    if(!newOperand){
-        if(expression[expression.length-1]==0){
-            expression.pop()
-        }
-        expression.push(currentOperand);
-        expression.push(currentOperator);
-        newOperand = true;
-        display.innerText = '';
-    }
-}
+// function makeExpression(e){
+//     let currentOperand = Number.parseInt(display.innerText);
+//     let currentOperator = '';
+//     switch(e.srcElement.dataset.operator){
+//         case '+': currentOperator = '+'; break;
+//         case '-': currentOperator = '-'; break;
+//         case '*': currentOperator = '*'; break;
+//         case '/': currentOperator = '/'; break;
+//     }
+//     if(!newOperand){
+//         if(expression[expression.length-1]==0){
+//             expression.pop()
+//         }
+//         expression.push(currentOperand);
+//         expression.push(currentOperator);
+//         newOperand = true;
+//         display.innerText = '';
+//     }
+// }
 
-function cleanExpression(){
-    switch(expression[expression.length-1]){
-        case '/':
-        case '*': 
-            if(display.innerText != ''){
-                expression.push(Number.parseInt(display.innerText));
-                display.innerText = '';
-            }else{
-                expression.push(1);
-            }
-            break;
-        case '+':
-        case '-': if(display.innerText != ''){
-            expression.push(Number.parseInt(display.innerText));
-            display.innerText = '';
-        }else{
-            expression.push(0);
-        }
-    }
-}
+// function cleanExpression(){
+//     switch(expression[expression.length-1]){
+//         case '/':
+//         case '*': 
+//             if(display.innerText != ''){
+//                 expression.push(Number.parseInt(display.innerText));
+//                 display.innerText = '';
+//             }else{
+//                 expression.push(1);
+//             }
+//             break;
+//         case '+':
+//         case '-': if(display.innerText != ''){
+//             expression.push(Number.parseInt(display.innerText));
+//             display.innerText = '';
+//         }else{
+//             expression.push(0);
+//         }
+//     }
+// }
 
-function solveExpression(){
-    // 6+3*4/2-1+10*2-5=26
-    let currentOperators = ['*','/','-','+']; //DO NOT CHANGE THE ORDER OF OPERATORS
+// function solveExpression(){
+//     // 6+3*4/2-1+10*2-5=26
+//     let currentOperators = ['*','/','-','+']; //DO NOT CHANGE THE ORDER OF OPERATORS
     
-    currentOperators.forEach(currentOperator=>{
-        for(let i = 0; i<expression.length; i++){
-            let result = 0;
-            if(expression[i]==currentOperator){
-                result = operate(currentOperator,expression[i-1],expression[i+1]);
-                expression.splice(i-1,3,result);
-                i=-1;
-            }
-        }
-    })
-}
+//     currentOperators.forEach(currentOperator=>{
+//         for(let i = 0; i<expression.length; i++){
+//             let result = 0;
+//             if(expression[i]==currentOperator){
+//                 result = operate(currentOperator,expression[i-1],expression[i+1]);
+//                 expression.splice(i-1,3,result);
+//                 i=-1;
+//             }
+//         }
+//     })
+// }
 
-function evaluateExpression(){
-    cleanExpression();
-    solveExpression();
-    display.innerText = expression[0];
-    expression = []
-}
+// function evaluateExpression(){
+//     cleanExpression();
+//     solveExpression();
+//     display.innerText = expression[0];
+//     expression = []
+// }
 
 let numBtns = document.querySelectorAll('.num-btn');
 let oprBtns = document.querySelectorAll('.opr-btn');
@@ -115,8 +115,8 @@ numBtns.forEach(numBtn=>{
 
 oprBtns.forEach(opr=>{
     if(opr.dataset.operator != 'IGNR'){
-        opr.addEventListener('click',makeExpression)   
+        opr.addEventListener('click',operate)   
     }
 })
 
-eql.addEventListener('click',evaluateExpression)
+eql.addEventListener('click',solve)
